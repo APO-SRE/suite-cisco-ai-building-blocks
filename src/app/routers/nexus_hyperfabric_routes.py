b8a3dddad6beeca1af4b961c922723f376130b71
Jeff Teeter, Ph.D <jefteete@cisco.com>
2025-06-19 03:33:14 +0000

    updated routes

--- src/app/routers/nexus_hyperfabric_routes.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from app.llm.unified_service import UnifiedService
-
-router = APIRouter(prefix="/nexus_hyperfabric", tags=["nexus_hyperfabric"])
-
-@router.get("/")
-async def get_nexus_hyperfabric_info():
-    return UnifiedService.nexus_hyperfabric_info()
