e272b4601e2eb183a9950487683ee5a91f917a6e
Jeff Teeter, Ph.D. <jefteete@cisco.com>
2025-06-19 19:46:43 +0000

    updated create_sdk.py to use the underlying generate_openapi_sdk.py script

--- src/app/llm/function_dispatcher/nexus_hyperfabric_dispatcher.py
+++ src/app/llm/function_dispatcher/nexus_hyperfabric_dispatcher.py
@@ -7,6 +7,14 @@ from app.llm.platform_clients.nexus_hyperfabric_client import Nexus_hyperfabricC
 def authGetBearerTokens(**kwargs):
     return Nexus_hyperfabricClient().authGetBearerTokens(**{**kwargs})
 
+@register('authCreateBearerTokens')
+def authCreateBearerTokens():
+    return Nexus_hyperfabricClient().authCreateBearerTokens(**{})
+
+@register('authDeleteBearerToken')
+def authDeleteBearerToken(tokenId: str):
+    return Nexus_hyperfabricClient().authDeleteBearerToken(**{'tokenId': tokenId})
+
 @register('authGetBearerToken')
 def authGetBearerToken(tokenId: str, **kwargs):
     return Nexus_hyperfabricClient().authGetBearerToken(**{'tokenId': tokenId, **kwargs})
@@ -19,21 +27,61 @@ def devicesGetDevices():
 def fabricsGetAllFabrics(**kwargs):
     return Nexus_hyperfabricClient().fabricsGetAllFabrics(**{**kwargs})
 
+@register('fabricsAddFabrics')
+def fabricsAddFabrics():
+    return Nexus_hyperfabricClient().fabricsAddFabrics(**{})
+
+@register('fabricsDeleteFabric')
+def fabricsDeleteFabric(fabricId: str):
+    return Nexus_hyperfabricClient().fabricsDeleteFabric(**{'fabricId': fabricId})
+
 @register('fabricsGetFabric')
 def fabricsGetFabric(fabricId: str, **kwargs):
     return Nexus_hyperfabricClient().fabricsGetFabric(**{'fabricId': fabricId, **kwargs})
+
+@register('fabricsUpdateFabric')
+def fabricsUpdateFabric(fabricId: str):
+    return Nexus_hyperfabricClient().fabricsUpdateFabric(**{'fabricId': fabricId})
 
 @register('fabricsGetFabricCandidates')
 def fabricsGetFabricCandidates(fabricId: str, **kwargs):
     return Nexus_hyperfabricClient().fabricsGetFabricCandidates(**{'fabricId': fabricId, **kwargs})
 
+@register('fabricsRevertFabricCandidate')
+def fabricsRevertFabricCandidate(fabricId: str, name: str):
+    return Nexus_hyperfabricClient().fabricsRevertFabricCandidate(**{'fabricId': fabricId, 'name': name})
+
 @register('fabricsGetFabricCandidate')
 def fabricsGetFabricCandidate(fabricId: str, name: str, **kwargs):
     return Nexus_hyperfabricClient().fabricsGetFabricCandidate(**{'fabricId': fabricId, 'name': name, **kwargs})
 
+@register('fabricsCommitFabricCandidate')
+def fabricsCommitFabricCandidate(fabricId: str, name: str):
+    return Nexus_hyperfabricClient().fabricsCommitFabricCandidate(**{'fabricId': fabricId, 'name': name})
+
+@register('fabricsReviewFabricCandidate')
+def fabricsReviewFabricCandidate(fabricId: str, name: str):
+    return Nexus_hyperfabricClient().fabricsReviewFabricCandidate(**{'fabricId': fabricId, 'name': name})
+
+@register('fabricsDeleteFabricConnections')
+def fabricsDeleteFabricConnections(fabricId: str):
+    return Nexus_hyperfabricClient().fabricsDeleteFabricConnections(**{'fabricId': fabricId})
+
 @register('fabricsGetFabricConnections')
 def fabricsGetFabricConnections(fabricId: str, **kwargs):
     return Nexus_hyperfabricClient().fabricsGetFabricConnections(**{'fabricId': fabricId, **kwargs})
+
+@register('fabricsAddFabricConnections')
+def fabricsAddFabricConnections(fabricId: str):
+    return Nexus_hyperfabricClient().fabricsAddFabricConnections(**{'fabricId': fabricId})
+
+@register('fabricsSetFabricConnections')
+def fabricsSetFabricConnections(fabricId: str):
+    return Nexus_hyperfabricClient().fabricsSetFabricConnections(**{'fabricId': fabricId})
+
+@register('fabricsDeleteFabricConnection')
+def fabricsDeleteFabricConnection(connectionId: str, fabricId: str):
+    return Nexus_hyperfabricClient().fabricsDeleteFabricConnection(**{'connectionId': connectionId, 'fabricId': fabricId})
 
 @register('fabricsGetFabricConnection')
 def fabricsGetFabricConnection(connectionId: str, fabricId: str, **kwargs):
@@ -43,37 +91,97 @@ def fabricsGetFabricConnection(connectionId: str, fabricId: str, **kwargs):
 def nodesGetFabricNodes(fabricId: str, **kwargs):
     return Nexus_hyperfabricClient().nodesGetFabricNodes(**{'fabricId': fabricId, **kwargs})
 
+@register('nodesAddFabricNodes')
+def nodesAddFabricNodes(fabricId: str):
+    return Nexus_hyperfabricClient().nodesAddFabricNodes(**{'fabricId': fabricId})
+
+@register('nodesDeleteFabricNode')
+def nodesDeleteFabricNode(fabricId: str, nodeId: str):
+    return Nexus_hyperfabricClient().nodesDeleteFabricNode(**{'fabricId': fabricId, 'nodeId': nodeId})
+
 @register('nodesGetNamedFabricNode')
 def nodesGetNamedFabricNode(fabricId: str, nodeId: str, **kwargs):
     return Nexus_hyperfabricClient().nodesGetNamedFabricNode(**{'fabricId': fabricId, 'nodeId': nodeId, **kwargs})
+
+@register('nodesUpdateFabricNode')
+def nodesUpdateFabricNode(fabricId: str, nodeId: str):
+    return Nexus_hyperfabricClient().nodesUpdateFabricNode(**{'fabricId': fabricId, 'nodeId': nodeId})
+
+@register('devicesUnbindDevice')
+def devicesUnbindDevice(fabricId: str, nodeId: str):
+    return Nexus_hyperfabricClient().devicesUnbindDevice(**{'fabricId': fabricId, 'nodeId': nodeId})
+
+@register('devicesBindDevice')
+def devicesBindDevice(deviceId: str, fabricId: str, nodeId: str):
+    return Nexus_hyperfabricClient().devicesBindDevice(**{'deviceId': deviceId, 'fabricId': fabricId, 'nodeId': nodeId})
 
 @register('nodesGetManagementPorts')
 def nodesGetManagementPorts(fabricId: str, nodeId: str, **kwargs):
     return Nexus_hyperfabricClient().nodesGetManagementPorts(**{'fabricId': fabricId, 'nodeId': nodeId, **kwargs})
 
+@register('nodesAddManagementPorts')
+def nodesAddManagementPorts(fabricId: str, nodeId: str):
+    return Nexus_hyperfabricClient().nodesAddManagementPorts(**{'fabricId': fabricId, 'nodeId': nodeId})
+
 @register('nodesGetManagementPort')
 def nodesGetManagementPort(fabricId: str, id: str, nodeId: str, **kwargs):
     return Nexus_hyperfabricClient().nodesGetManagementPort(**{'fabricId': fabricId, 'id': id, 'nodeId': nodeId, **kwargs})
+
+@register('nodesUpdateManagementPort')
+def nodesUpdateManagementPort(fabricId: str, id: str, nodeId: str):
+    return Nexus_hyperfabricClient().nodesUpdateManagementPort(**{'fabricId': fabricId, 'id': id, 'nodeId': nodeId})
 
 @register('nodesGetPorts')
 def nodesGetPorts(fabricId: str, nodeId: str, **kwargs):
     return Nexus_hyperfabricClient().nodesGetPorts(**{'fabricId': fabricId, 'nodeId': nodeId, **kwargs})
 
+@register('nodesSetPorts')
+def nodesSetPorts(fabricId: str, nodeId: str):
+    return Nexus_hyperfabricClient().nodesSetPorts(**{'fabricId': fabricId, 'nodeId': nodeId})
+
+@register('nodesResetPort')
+def nodesResetPort(fabricId: str, nodeId: str, portId: str):
+    return Nexus_hyperfabricClient().nodesResetPort(**{'fabricId': fabricId, 'nodeId': nodeId, 'portId': portId})
+
 @register('nodesGetPort')
 def nodesGetPort(fabricId: str, nodeId: str, portId: str, **kwargs):
     return Nexus_hyperfabricClient().nodesGetPort(**{'fabricId': fabricId, 'nodeId': nodeId, 'portId': portId, **kwargs})
+
+@register('nodesUpdatePort')
+def nodesUpdatePort(fabricId: str, nodeId: str, portId: str):
+    return Nexus_hyperfabricClient().nodesUpdatePort(**{'fabricId': fabricId, 'nodeId': nodeId, 'portId': portId})
 
 @register('vnisGetFabricVnis')
 def vnisGetFabricVnis(fabricId: str, **kwargs):
     return Nexus_hyperfabricClient().vnisGetFabricVnis(**{'fabricId': fabricId, **kwargs})
 
+@register('vnisAddFabricVnis')
+def vnisAddFabricVnis(fabricId: str):
+    return Nexus_hyperfabricClient().vnisAddFabricVnis(**{'fabricId': fabricId})
+
+@register('vnisDeleteFabricVni')
+def vnisDeleteFabricVni(fabricId: str, vniId: str):
+    return Nexus_hyperfabricClient().vnisDeleteFabricVni(**{'fabricId': fabricId, 'vniId': vniId})
+
 @register('vnisGetFabricVni')
 def vnisGetFabricVni(fabricId: str, vniId: str, **kwargs):
     return Nexus_hyperfabricClient().vnisGetFabricVni(**{'fabricId': fabricId, 'vniId': vniId, **kwargs})
 
+@register('vnisUpdateFabricVni')
+def vnisUpdateFabricVni(fabricId: str, vniId: str):
+    return Nexus_hyperfabricClient().vnisUpdateFabricVni(**{'fabricId': fabricId, 'vniId': vniId})
+
 @register('vnisGetFabricVniMembers')
 def vnisGetFabricVniMembers(fabricId: str, vniId: str, **kwargs):
     return Nexus_hyperfabricClient().vnisGetFabricVniMembers(**{'fabricId': fabricId, 'vniId': vniId, **kwargs})
+
+@register('vnisAddFabricVniMembers')
+def vnisAddFabricVniMembers(fabricId: str, vniId: str):
+    return Nexus_hyperfabricClient().vnisAddFabricVniMembers(**{'fabricId': fabricId, 'vniId': vniId})
+
+@register('vnisDeleteFabricVniMember')
+def vnisDeleteFabricVniMember(fabricId: str, memberId: str, vniId: str):
+    return Nexus_hyperfabricClient().vnisDeleteFabricVniMember(**{'fabricId': fabricId, 'memberId': memberId, 'vniId': vniId})
 
 @register('vnisGetFabricVniMember')
 def vnisGetFabricVniMember(fabricId: str, memberId: str, vniId: str, **kwargs):
@@ -83,22 +191,58 @@ def vnisGetFabricVniMember(fabricId: str, memberId: str, vniId: str, **kwargs):
 def vrfsGetFabricVrfs(fabricId: str, **kwargs):
     return Nexus_hyperfabricClient().vrfsGetFabricVrfs(**{'fabricId': fabricId, **kwargs})
 
+@register('vrfsAddFabricVrfs')
+def vrfsAddFabricVrfs(fabricId: str):
+    return Nexus_hyperfabricClient().vrfsAddFabricVrfs(**{'fabricId': fabricId})
+
+@register('vrfsDeleteFabricVrf')
+def vrfsDeleteFabricVrf(fabricId: str, vrfId: str):
+    return Nexus_hyperfabricClient().vrfsDeleteFabricVrf(**{'fabricId': fabricId, 'vrfId': vrfId})
+
 @register('vrfsGetFabricVrf')
 def vrfsGetFabricVrf(fabricId: str, vrfId: str, **kwargs):
     return Nexus_hyperfabricClient().vrfsGetFabricVrf(**{'fabricId': fabricId, 'vrfId': vrfId, **kwargs})
+
+@register('vrfsUpdateFabricVrf')
+def vrfsUpdateFabricVrf(fabricId: str, vrfId: str):
+    return Nexus_hyperfabricClient().vrfsUpdateFabricVrf(**{'fabricId': fabricId, 'vrfId': vrfId})
 
 @register('vrfsGetFabricStaticRoutes')
 def vrfsGetFabricStaticRoutes(fabricId: str, vrfId: str, **kwargs):
     return Nexus_hyperfabricClient().vrfsGetFabricStaticRoutes(**{'fabricId': fabricId, 'vrfId': vrfId, **kwargs})
 
+@register('vrfsAddFabricStaticRoutes')
+def vrfsAddFabricStaticRoutes(fabricId: str, vrfId: str):
+    return Nexus_hyperfabricClient().vrfsAddFabricStaticRoutes(**{'fabricId': fabricId, 'vrfId': vrfId})
+
+@register('vrfsDeleteFabricStaticRoute')
+def vrfsDeleteFabricStaticRoute(fabricId: str, routeId: str, vrfId: str):
+    return Nexus_hyperfabricClient().vrfsDeleteFabricStaticRoute(**{'fabricId': fabricId, 'routeId': routeId, 'vrfId': vrfId})
+
 @register('vrfsGetFabricStaticRoute')
 def vrfsGetFabricStaticRoute(fabricId: str, routeId: str, vrfId: str, **kwargs):
     return Nexus_hyperfabricClient().vrfsGetFabricStaticRoute(**{'fabricId': fabricId, 'routeId': routeId, 'vrfId': vrfId, **kwargs})
+
+@register('vrfsUpdateFabricStaticRoute')
+def vrfsUpdateFabricStaticRoute(fabricId: str, routeId: str, vrfId: str):
+    return Nexus_hyperfabricClient().vrfsUpdateFabricStaticRoute(**{'fabricId': fabricId, 'routeId': routeId, 'vrfId': vrfId})
 
 @register('authGetUsers')
 def authGetUsers(**kwargs):
     return Nexus_hyperfabricClient().authGetUsers(**{**kwargs})
 
+@register('authSetUsers')
+def authSetUsers():
+    return Nexus_hyperfabricClient().authSetUsers(**{})
+
+@register('authDeleteUser')
+def authDeleteUser(userId: str):
+    return Nexus_hyperfabricClient().authDeleteUser(**{'userId': userId})
+
 @register('authGetUser')
 def authGetUser(userId: str, **kwargs):
     return Nexus_hyperfabricClient().authGetUser(**{'userId': userId, **kwargs})
+
+@register('authUpdateUser')
+def authUpdateUser(userId: str):
+    return Nexus_hyperfabricClient().authUpdateUser(**{'userId': userId})
--- src/app/user_commands/create_sdk.py
+++ src/app/user_commands/create_sdk.py
@@ -7,33 +7,17 @@ from __future__ import annotations
 ## Licensed under the Apache License, Version 2.0 (see LICENSE)
 ## Distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND.
 #######################################################################################
-"""
-╔═══════════════════ OpenAPI SDK Generation Wizard ═════════════════════════╗
-║ A polished step‑by‑step CLI to run openapi‑python-client for your specs.  ║
-║                                                                           ║
-║ FEATURES                                                                  ║
-║   • Select a spec                                                         ║
-║   • Suggest folder name from platform_registry.json                       ║
-║   • Generate the SDK with poetry meta                                     ║
-║   • Auto‑populate *sdk_module* in platform_registry.json                  ║
-║     (but **does NOT touch the `installed` flag**)                         ║
-║   • Show pip‑install instructions & import snippet                        ║
-║   • Auto‑install generated SDK in editable mode                           ║
-║   • Reorganize "api" folder into "apis/tags" to match dynamic loader     ║
-╚══════════════════════════════════════════════════════════════════════════╝
-"""
 
 import os
 import sys
-import shlex
 import subprocess
-import json
-import re
-import tomllib
-import shutil
-import yaml
 from pathlib import Path
 from typing import List, Dict, Optional
+import json
+import re
+import shutil
+import tomllib
+import yaml
 
 from rich.console import Console
 from rich.panel import Panel
@@ -44,235 +28,160 @@ from rich.markdown import Markdown
 from rich import box
 from rich.traceback import install
 
+# ─────────────────────────────────────────────────────────────────────────────
 install()
 console = Console()
 
-# ────────────────────────────────────────────────────────────────────────────────
-# Constants
-# ────────────────────────────────────────────────────────────────────────────────
-PROJECT_ROOT           = Path(__file__).resolve().parents[3]
-SOURCE_DIR             = PROJECT_ROOT / "src" / "source_open_api"
-OUTPUT_BASE_DIR        = PROJECT_ROOT / "src" / "db_scripts" / "output_sdk"
-PLATFORM_REGISTRY_FILE = PROJECT_ROOT / "src" / "app" / "llm" / "platform_registry.json"
+# ──────────────────────────── Paths / constants
+PROJECT_ROOT = Path(__file__).resolve().parents[3]
+SOURCE_DIR   = PROJECT_ROOT / "src" / "source_open_api"
+OUTPUT_BASE  = PROJECT_ROOT / "src" / "db_scripts" / "output_sdk"
 
-# ────────────────────────────────────────────────────────────────────────────────
-# Helpers
-# ────────────────────────────────────────────────────────────────────────────────
+# We will *call into* the bulk generator, so import it lazily
+GENERATOR_MOD = "db_scripts.scripts.generate_openapi_sdks"
 
+# ──────────────────────────── Helper utilities
 def clear_screen() -> None:
     if sys.stdout.isatty():
         os.system("cls" if os.name == "nt" else "clear")
 
 
-def list_existing_sdks() -> List[str]:
-    return sorted(p.name for p in OUTPUT_BASE_DIR.iterdir() if p.is_dir()) if OUTPUT_BASE_DIR.exists() else []
-
-
 def list_specs() -> List[Path]:
-    specs = sorted(
+    return sorted(
         p for p in SOURCE_DIR.iterdir()
         if p.is_file() and p.suffix.lower() in {".json", ".yaml", ".yml"}
     )
-    if not specs:
-        console.print(f"[red]No OpenAPI files found in {SOURCE_DIR}[/red]")
-        sys.exit(1)
-    return specs
 
 
-def ask_choice(prompt: str, default: str) -> str:
-    return Prompt.ask(f"[cyan]?[/cyan] [bold]{prompt}[/bold]", default=default).strip()
+def list_existing_sdks() -> List[str]:
+    return sorted(p.name for p in OUTPUT_BASE.iterdir() if p.is_dir()) if OUTPUT_BASE.exists() else []
 
 
-def sanitize_sdk_name(name: str) -> str:
-    sanitized = re.sub(r"[^0-9a-zA-Z_]+", "_", name).lower()
-    sanitized = re.sub(r"_+", "_", sanitized).strip("_")
-    return sanitized or "sdk"
+def ask(prompt: str, default: str | None = None) -> str:
+    return Prompt.ask(f"[cyan]?[/cyan] [bold]{prompt}[/bold]", default=default or "").strip()
 
 
-def build_command(spec: Path, sdk_name: str, package_name: str) -> List[str]:
-    dest = OUTPUT_BASE_DIR / sdk_name
-    dest.mkdir(parents=True, exist_ok=True)
-
-    cfg_path = dest / ".openapi-config.yml"
-    cfg_data = {
-        "project_name_override": package_name,
-        "package_name_override": package_name,
-    }
-    cfg_path.write_text(yaml.safe_dump(cfg_data), encoding="utf-8")
-
-    return [
-        "openapi-python-client",
-        "generate",
-        "--path",
-        str(spec),
-        "--output-path",
-        str(dest),
-        "--meta",
-        "poetry",
-        "--overwrite",
-        "--config",
-        str(cfg_path),
-    ]
+def sanitize_folder(name: str) -> str:
+    out = re.sub(r"[^0-9a-zA-Z_]+", "_", name).lower()
+    return re.sub(r"_+", "_", out).strip("_") or "sdk"
 
 
-def parse_package_dir(dest_dir: Path, fallback: str) -> str:
-    pyproject = dest_dir / "pyproject.toml"
-    if pyproject.exists():
-        try:
-            data = tomllib.loads(pyproject.read_text(encoding="utf-8"))
-            poetry = data.get("tool", {}).get("poetry", {})
-            name = poetry.get("name") or data.get("project", {}).get("name")
-            if isinstance(name, str):
-                return name.replace("-", "_")
-        except Exception:
-            pass
-    for p in dest_dir.iterdir():
-        if p.is_dir() and (p / "__init__.py").exists():
-            return p.name.replace("-", "_")
-    return fallback.replace("-", "_")
-
-
-def load_registry() -> Dict[str, Dict[str, str]]:
-    try:
-        return json.loads(PLATFORM_REGISTRY_FILE.read_text(encoding="utf-8"))
-    except Exception:
-        return {}
-
-
-def save_registry(reg: Dict[str, Dict[str, str]]) -> None:
-    PLATFORM_REGISTRY_FILE.parent.mkdir(parents=True, exist_ok=True)
-    PLATFORM_REGISTRY_FILE.write_text(json.dumps(reg, indent=2), encoding="utf-8")
-
-
-def registry_short_name_for_spec(spec: Path, registry: Dict[str, Dict[str, str]]) -> Optional[str]:
-    stem = spec.stem.lower()
-    for short_name, entry in registry.items():
-        if stem == str(entry.get("openapi_name", "")).lower():
-            return short_name
-    return None
-
-# ────────────────────────────────────────────────────────────────────────────────
-# Main CLI
-# ────────────────────────────────────────────────────────────────────────────────
-
+# ──────────────────────────── Main CLI
 def main() -> None:
-    if shutil.which("openapi-python-client") is None:
-        console.print(
-            "[red]`openapi-python-client` is not installed. "
-            "Install it with `pip install openapi-python-client` and try again.[/red]"
-        )
-        sys.exit(1)
     clear_screen()
     console.print(Panel.fit("🚀 OpenAPI SDK Generation Wizard", style="green"))
 
-    console.print(Panel(
-        ", ".join(list_existing_sdks()) or "(none)",
-        title="Existing SDKs in output_sdk/",
-        border_style="magenta",
-    ))
+    # Show already‑generated SDKs
+    console.print(
+        Panel(
+            ", ".join(list_existing_sdks()) or "(none)",
+            title="Existing SDKs in output_sdk/",
+            border_style="magenta",
+        )
+    )
 
     specs = list_specs()
-    table = Table(box=box.SIMPLE_HEAVY)
-    table.add_column("#", style="bold cyan")
-    table.add_column("Spec file")
-    for i, path in enumerate(specs, 1):
-        table.add_row(str(i), path.name)
-    exit_idx = len(specs) + 1
-    table.add_row(str(exit_idx), "Exit")
-    console.print(Panel(table, title="Step 1/4: Select OpenAPI Spec", border_style="cyan"))
+    if not specs:
+        console.print(f"[red]No OpenAPI documents found in {SOURCE_DIR}[/red]")
+        sys.exit(1)
 
-    choice = ask_choice(f"Enter number [1-{exit_idx}]", "")
-    if choice.isdigit() and int(choice) == exit_idx:
-        console.print("[green]Exiting…[/green]")
+    # Step 1 – choose spec
+    table = Table(box=box.SIMPLE_HEAVY, show_lines=False)
+    table.add_column("#", style="bold cyan", width=4)
+    table.add_column("OpenAPI document")
+    for i, sp in enumerate(specs, 1):
+        table.add_row(str(i), sp.name)
+    table.add_row("0", "Exit")
+    console.print(Panel(table, title="Select an OpenAPI spec", border_style="cyan"))
+
+    choice = ask(f"Enter number [1‑{len(specs)}]", "")
+    if choice == "0":
+        console.print("[green]Bye![/green]")
         sys.exit(0)
-    if not (choice.isdigit() and 1 <= (idx := int(choice)) <= len(specs)):
-        console.print("[red]Invalid selection. Exiting.[/red]")
+    if not choice.isdigit() or not (1 <= int(choice) <= len(specs)):
+        console.print("[red]Invalid selection[/red]")
         sys.exit(1)
-    spec = specs[idx - 1]
-    console.print(f":white_check_mark: Selected [bold]{spec.name}[/bold]\n")
 
-    registry = load_registry()
-    default_sdk_name = registry_short_name_for_spec(spec, registry) or ""
-    console.print(Panel.fit(
-        "🔧 Step 2/4: Name Your SDK Folder" + (f"\n[dim](suggested: {default_sdk_name})[/dim]" if default_sdk_name else ""),
-        border_style="cyan",
-    ))
-    sdk_name = Prompt.ask("[cyan]?[/cyan] [bold]Enter a short SDK folder name[/bold]", default=default_sdk_name).strip()
-    if not sdk_name:
-        console.print("[red]You must enter a non-empty SDK name. Exiting.[/red]")
-        sys.exit(1)
-    package_name = sanitize_sdk_name(sdk_name)
-    console.print(":white_check_mark: Name set\n")
+    spec_path: Path = specs[int(choice) - 1]
+    console.print(f":white_check_mark: Selected [bold]{spec_path.name}[/bold]\n")
 
-    cmd = build_command(spec, sdk_name, package_name)
-    console.print(Panel(Markdown(f"**Command to run**\n```bash {' '.join(shlex.quote(p) for p in cmd)} ```"), title="Step 3/4: Preview Command", border_style="cyan"))
-    if ask_choice("Proceed with SDK generation? (Y/n)", "Y").lower().startswith("n"):
-        console.print("[yellow]Aborted by user.[/yellow]")
-        return
+    # Step 2 – folder / package name
+    sdk_folder_raw = ask("Enter folder name for the SDK (will live under output_sdk/)", spec_path.stem)
+    sdk_folder     = sanitize_folder(sdk_folder_raw)
+    console.print(f":white_check_mark: Folder will be [bold]{sdk_folder}[/bold]\n")
 
-    result = subprocess.run(cmd)
+    # Confirm
+    if ask("Proceed with SDK generation? (Y/n)", "Y").lower().startswith("n"):
+        console.print("[yellow]Aborted by user[/yellow]")
+        sys.exit(0)
+
+    # ----------------------------------------------------------------------
+    # DELEGATE to the bulk generator
+    # We run it in a *sub‑process* so that its interactive prompts can be
+    # faked with stdin.  We provide our two answers (spec index + folder).
+    # ----------------------------------------------------------------------
+    import importlib.util
+
+    # 1. Build the command that resolves to the proper entry‑point script
+    generator_entry = Path(importlib.util.find_spec(GENERATOR_MOD).origin)
+
+    cmd = [
+        sys.executable,
+        str(generator_entry),
+    ]
+
+    # 2. Pre‑feed answers to the generator over stdin
+    #
+    #   • First prompt  → choose the single spec index
+    #   • Second prompt → folder name
+    #
+    answers = f"{choice}\n{sdk_folder}\n"
+    result = subprocess.run(cmd, input=answers.encode(), cwd=PROJECT_ROOT)
     if result.returncode != 0:
-        console.print(f"[red]Generation failed for {spec.name}[/red]")
+        console.print(f"[red]Generator script failed (exit {result.returncode})[/red]")
         sys.exit(1)
-    console.print(f"[green]Success for {spec.name} → {sdk_name}[/green]\n")
 
-    # Sanitize version in pyproject.toml
-    pyproject_toml = OUTPUT_BASE_DIR / sdk_name / "pyproject.toml"
-    if pyproject_toml.exists():
-        content = pyproject_toml.read_text(encoding="utf-8")
-        sanitized = re.sub(
-            r'^(version\s*=\s*"\d+\.\d+\.\d+\.post)([0-9\.]+)"',
-            lambda m: f'{m.group(1)}{m.group(2).replace(".", "")}"',
-            content,
-            flags=re.MULTILINE,
-        )
-        if sanitized != content:
-            pyproject_toml.write_text(sanitized, encoding="utf-8")
-            console.print("[grey]→ sanitized version in pyproject.toml[/grey]")
+    # ----------------------------------------------------------------------
+    # Post‑generation: install editable package (same as before)
+    # ----------------------------------------------------------------------
+    edit_path = OUTPUT_BASE / sdk_folder
+    console.print(Panel.fit("📦 Installing SDK in editable mode…", style="cyan"))
+    subprocess.run([sys.executable, "-m", "pip", "install", "-e", str(edit_path)])
 
-    # Reorganize api modules into apis/tags for unified import paths
-    pkg_dir = parse_package_dir(OUTPUT_BASE_DIR / sdk_name, sdk_name)
-    sdk_pkg = OUTPUT_BASE_DIR / sdk_name / pkg_dir
-    api_src = sdk_pkg / "api"
-    api_dst = sdk_pkg / "apis" / "tags"
-    if api_src.exists():
-        api_dst.mkdir(parents=True, exist_ok=True)
-        for f in api_src.glob("*_api.py"):
-            shutil.move(str(f), api_dst / f.name)
-        shutil.rmtree(api_src)
-        console.print("[grey]→ moved api/*.py to apis/tags and cleaned up[/grey]")
-
-    # Auto-install generated SDK
-    install_cmd = [sys.executable, "-m", "pip", "install", "-e", str(OUTPUT_BASE_DIR / sdk_name)]
-    console.print(Panel.fit("🚀 Installing generated SDK…", style="cyan"))
-    subprocess.run(install_cmd)
-    console.print(f"[grey]→ ran: {' '.join(shlex.quote(p) for p in install_cmd)}[/grey]")
-
-    # Show output_sdk tree
-    tree = Tree(f"[bold]{OUTPUT_BASE_DIR.name}[/bold]")
-    for d in sorted(OUTPUT_BASE_DIR.iterdir()):
+    # Show tree
+    tree = Tree(f"[bold]{OUTPUT_BASE.name}[/bold]")
+    for d in sorted(OUTPUT_BASE.iterdir()):
         if d.is_dir():
             br = tree.add(d.name)
             for c in sorted(d.iterdir()):
                 br.add(c.name)
     console.print(tree)
 
-    # Update registry
-    short = registry_short_name_for_spec(spec, registry) or sdk_name
-    entry = registry.get(short, {})
-    entry["openapi_name"] = spec.stem
-    entry["sdk_module"] = pkg_dir
-    entry["created_by_us"] = True
-    registry[short] = entry
-    save_registry(registry)
-    console.print(f"[green]Registry updated: {short} → sdk_module={pkg_dir}[/green]")
+    # Usage hint
+    # Guess the package dir from pyproject.toml
+    pyproject = edit_path / "pyproject.toml"
+    pkg_name  = sdk_folder
+    if pyproject.exists():
+        data = tomllib.loads(pyproject.read_text("utf-8"))
+        pkg_name = (
+            data.get("project", {}).get("name")
+            or data.get("tool", {}).get("poetry", {}).get("name")
+            or sdk_folder
+        ).replace("-", "_")
 
-    console.print(Panel.fit("📦 Usage in your Python code:", style="cyan"))
-    console.print(Markdown(f"```python\nfrom {pkg_dir}.client import Client\n```"))
+    console.print(
+        Panel.fit(
+            Markdown(f"```python\nfrom {pkg_name}.client import Client\n```"),
+            title="✅ SDK ready to import",
+            border_style="green",
+        )
+    )
 
+
+# ────────────────────────────
 if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
-        console.print("\n[red]Interrupted by user.[/red]")
+        console.print("\n[red]Interrupted[/red]")
         sys.exit(1)
